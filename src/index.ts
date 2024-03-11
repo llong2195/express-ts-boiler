1162031f7bd662491cdd38db9cbf82af0fac5b04
vungocthien <vnthien3012@gmail.com>
2024-03-11 23:02:00 +0700

    fistcommit

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,14 @@ import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import HttpError from './common/http.error';
 import UserRouter from './modules/auth/auth.router';
+import path from 'path';
+import { encodeHLSWithMultipleVideoStreams } from './service/service';
+import { fs } from 'zx/.';
 
 config();
 const PORT = process.env.PORT || 5000;
+
+const staticFilesPath = path.join(__dirname, '../videos');
 
 async function main() {
   const app = express();
@@ -37,6 +42,69 @@ async function main() {
       data: null,
       message: err.message || 'Something went wrong',
     });
+  });
+
+  app.use(
+    express.static(staticFilesPath, {
+      extensions: ['m3u8', 'ts'],
+    }),
+  );
+
+  app.use(cors());
+
+  app.get('/convert:filename', (req: Request, res: Response, next: NextFunction) => {
+    try {
+      var filename = req.params.filename;
+      var filePath = path.join(__dirname, '../videos/' + filename);
+      if (!filePath) {
+        return res.status(404).send('File not found!!');
+      }
+      encodeHLSWithMultipleVideoStreams(filePath);
+    } catch (ex) {
+      throw new Error(ex);
+    }
+  });
+
+  app.get('/streaming/:filename', (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const fileName = req.params.filename;
+      const filePath = path.join(__dirname, '../videos/v1' + fileName);
+      console.log({ filePath });
+      if (!filePath) {
+        return res.status(404).send('file not found');
+      }
+      const stat = fs.statSync(filePath);
+      const fileSize = stat.size;
+      const range = req.headers.range;
+      if (range) {
+        const parts = range.replace(/bytes=/, '').split('-');
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+        const chuckSize = end - start + 1;
+
+        const file = fs.createReadStream(filePath, { start, end });
+        const head = {
+          'Content-Range': `bytes ${start} - ${end}/ ${fileSize}`,
+          'Accept-Range': `bytes`,
+          'Content-Lenght': chuckSize,
+          'Cpntent-Type': 'video/m3u8',
+        };
+        res.writeHead(206, head);
+        console.log({ head });
+        file.pipe(res);
+      } else {
+        const head = {
+          'Content-Lenght': fileSize,
+          'Cpntent-Type': 'video/m3u8',
+        };
+        console.log({ head });
+        res.writeHead(200, head);
+        fs.createReadStream(filePath).pipe(res);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
   });
 
   app.listen(PORT, () => {
